871ae90ae5b79fb39f31f0867c44c35f435b0dc9
xingxing <823952375@qq.com>
2023-05-04 11:08:21 +0800

    去掉搭建教程等信息

--- src/system.ts
+++ src/system.ts
@@ -11,10 +11,6 @@ export const defaultSetting = {
 }
 
 export const defaultMessage = `Powered by OpenAI Vercel
-- 本网站是由xingstarx搭建部署的，参考的是 Github开源社区的一个项目，[chatgpt-vercel](https://github.com/ourongxing/chatgpt-vercel) 要感谢作者ourongxing搞了一个这么好的开源项目。
-- 如果你也想要搭建一个属于自己的ChatGPT，可以参考这篇文章[手把手教你搭建一个属于自己的ChatGPT
-](https://www.jianshu.com/p/998c723443bd)
-- 如果你是技术人员，可以参考上面我写的文章自己搭建一个。
 - 如果你不懂技术也想要搭建一个类似的，那么可以出技术服务费找我，我帮你搞定技术问题，提供后续的技术支持(不支持定制化需求)，你只需要提供API Key,以及域名(可代买)，可详谈。
 - 也支持部署类似这张图[ChatGPT-Next-Web](./chatgpt_0_2.jpg)的效果哦，可以通过授权码访问的，防止白嫖，用来收费哦。
 - 如果你想要更进一步的了解ChatGPT的使用,变现,最新资讯等可以考虑一下这个小专栏[ChatGPT从0到1](./chatgpt_0_1.jpg) 这是一个技术朋友搞的，很便宜的价格，绝对物超所值，只需要10块钱。
